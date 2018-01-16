06bb94f779ddb7ccbca12a460836e9e71eb8db2f
jiyuu-jin <jiyuu.llc@gmail.com>
2018-01-16 14:55:24 -0500

    Circles Man

--- script.js
+++ script.js
@@ -52,6 +52,8 @@
             setInterval( randomPoint, 4000 );
 
             setInterval( loop, 1000 / 60 );
+
+            drawCircle();
 		}
 	}
 
@@ -155,3 +157,17 @@
         var element = document.getElementById('Interface');
         element.style.display = 'none';
     }
+
+    function drawCircle(){
+        var centerX = canvas.width / 2;
+        var centerY = canvas.height / 2;
+        var radius = 70;
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'red';
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = '#003300';
+        context.stroke();
+        context.closePath();
+	}
